90b44152300586ce53a5d50ed405c54b3dd00cfb
Sudesh Jha <sudeshjha2005@gmail.com>
2026-07-17 19:55:17 +0530

    fix bug in cabin row delete modal

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -100,13 +100,13 @@ const CabinRow = ({ cabin }) => {
             <CreateCabinForm cabinToEdit={cabin} />
           </Modal.Window>
 
-          <Modal.Open>
+          <Modal.Open opens="delete">
             <button>
               <HiTrash />
             </button>
           </Modal.Open>
 
-          <Modal.Window>
+          <Modal.Window name="delete">
             <ConfirmDelete
               resourceName="cabin"
               disabled={isDeleting}
