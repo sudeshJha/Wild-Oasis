91505403b58d86a4d23d7b3a2055fd5639450ba3
Sudesh Jha <sudeshjha2005@gmail.com>
2026-07-20 23:33:39 +0530

    user logout

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useLogout } from "./useLogout";
+import ButtonIcon from "../../ui/ButtonIcon";
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import SpinnerMini from "../../ui/SpinnerMini";
+
+const Logout = () => {
+  const { logout, isLoading } = useLogout();
+
+  return (
+    <ButtonIcon disabled={isLoading} onClick={logout}>
+      {!isLoading ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
+    </ButtonIcon>
+  );
+};
+
+export default Logout;
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -7,7 +7,6 @@ export async function login({ email, password }) {
   });
 
   if (error) throw new Error(error.message);
-  console.log(data);
   return data;
 }
 
@@ -19,4 +18,9 @@ export async function getCurrentUser() {
   const { data, error } = await supabase.auth.getUser();
   if (error) throw new Error(error.message);
   return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+  if (error) throw new Error(error.message);
 }
